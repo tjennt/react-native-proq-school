6e605e984e6defab847a5b0a6af65588ce345ee0
tienntps09110 <natriwit675111@gmail.com>
2020-10-03 19:16:54 +0700

    add react-native-element

--- src/sreens/HomeScreen.js
+++ src/sreens/HomeScreen.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { 
-    View, 
-    Text
-  } from 'react-native';
 
+import { Button, Text, ThemeProvider, Avatar, Badge, Header } from 'react-native-elements';
 import STYLE_GOBAL from '../styles/Global';
 
 export default class HomeScreen extends React.Component {
+    
     render() {
       return (
-        <View style={STYLE_GOBAL.container}>
-          <Text>HomeScreen</Text>
-        </View>
+        <ThemeProvider style={STYLE_GOBAL.container}>
+          <Header
+            centerComponent={{ text: 'MY TITLE', style: { color: '#fff', fontWeight: 'bold' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+            backgroundColor="#2a4054"
+          />
+         <Avatar
+            rounded
+            source={{
+              uri: 'https://randomuser.me/api/portraits/men/41.jpg',
+            }}
+            size="large"
+          />
+        </ThemeProvider>
       )
     }
   }
