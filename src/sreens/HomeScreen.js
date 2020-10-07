e9582fef34984ac7701e72066a677acce157fb48
tienntps09110 <natriwit675111@gmail.com>
2020-10-07 20:40:35 +0700

    add home screen

--- src/sreens/HomeScreen.js
+++ src/sreens/HomeScreen.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { Button, Text, ThemeProvider, Avatar, SearchBar } from 'react-native-elements';
+import { View, StyleSheet, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
+import { Button, Text, ThemeProvider, ListItem, Avatar, SearchBar } from 'react-native-elements';
 import STYLE_GOBAL from '../styles';
-
-// IMPORT 
-import { TabView, SceneMap } from 'react-native-tab-view';
 
 // IMPORT HEADER
 import Header from '../components/headers/DefaultHeaderComponent';
@@ -13,48 +10,178 @@ import TestComponent from '../components/TestComponent';
 // IMPORT LOCALE
 import { APP, NAVIGATOR } from '../constants/Locale';
 
+import * as COLORS from '../constants/Colors';
 
-const initialLayout = { width: Dimensions.get('window').width };
 
-const layoutRender = () => (
-  <TestComponent title="CHILDREN" />
-)
 export default class HomeScreen extends React.Component {
   
-  state = {
-    search: '',
-    index: 0,
-    routes: [],
-    renderScene: {
-      music: layoutRender,
-      albums: layoutRender,
-      recents: layoutRender,
-      purchased: layoutRender
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: '',
+      searchLoading: false,
+      categories: [],
+      news: []
     }
-  };
+  }
 
-
-  componentDidMount () {
+  componentDidMount() {
     this.setState({
-      routes: [
-        { key: 'music', title: 'Music' },
-        { key: 'albums', title: 'Albums' },
-        { key: 'recents', title: 'Recents' },
-        { key: 'purchased', title: 'Purchased' },
+      categories: [
+        {
+          id: 1,
+          name: 'Thông báo'
+        },
+        {
+          id: 1,
+          name: 'Sự kiện'
+        },
+        {
+          id: 1,
+          name: 'Học phí'
+        }
+      ],
+      news: [
+        {
+          id: 1,
+          name: 'Thông báo đóng học phí',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 2,
+          name: 'Thông báo đewqsdss',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 3,
+          name: 'Thông báo Nghỉ học',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 4,
+          name: 'Liên hoang trường đi',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 5,
+          name: 'Hello mấy cưng',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 6,
+          name: 'Nghỉ nha khỏi học hành gì hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 7,
+          name: 'Học quan gi, nghỉ chấm hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 3,
+          name: 'Thông báo Nghỉ học',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 4,
+          name: 'Liên hoang trường đi',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 5,
+          name: 'Hello mấy cưng',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 6,
+          name: 'Nghỉ nha khỏi học hành gì hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 7,
+          name: 'Học quan gi, nghỉ chấm hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 3,
+          name: 'Thông báo Nghỉ học',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 4,
+          name: 'Liên hoang trường đi',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 5,
+          name: 'Hello mấy cưng',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 6,
+          name: 'Nghỉ nha khỏi học hành gì hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 7,
+          name: 'Học quan gi, nghỉ chấm hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 3,
+          name: 'Thông báo Nghỉ học',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 4,
+          name: 'Liên hoang trường đi',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 5,
+          name: 'Hello mấy cưng',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 6,
+          name: 'Nghỉ nha khỏi học hành gì hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        },
+        {
+          id: 7,
+          name: 'Học quan gi, nghỉ chấm hết',
+          description: 'Hoc khong bao nhieu dong tien thi nhieu...'
+        }
       ]
     })
   }
-  
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
-  
-  renderScene = SceneMap(this.state.renderScene);
 
-  setIndex = index => this.setState({ index });
 
+  // Search
+  updateSearch = (text) => {
+    this.setState({
+      searchLoading: true,
+      search: text
+    })
+
+    setTimeout(()=> {
+      this.setState({
+        searchLoading: false
+      })
+    }, 2000)
+  }
+
+
+  // Selected button
+  buttonSeleted = (index) => {
+    if (index == 0) {
+      return styles.ButtonStyleSelected
+    }
+    return styles.ButtonStyle
+  }
+  
   render() {
-    const { search, index, routes } = this.state;
+    const { search, searchLoading, categories, news } = this.state;
     return (
       <ThemeProvider>
         <Header title={ NAVIGATOR.home } />
@@ -63,18 +190,73 @@ export default class HomeScreen extends React.Component {
           onChangeText={this.updateSearch}
           value={search}
           platform="android"
-          showLoading = { true }
+          showLoading = { searchLoading }
         />
         <Text>{ search }</Text>
-        <TabView
-          navigationState={{ index, routes }}
-          renderScene={this.renderScene}
-          onIndexChange={this.setIndex}
-          initialLayout={initialLayout}
-          contentContainerStyle = {{ backgroundColor: 'red', color: 'red' }}
-        />
+        <View style={ { flexDirection: "column" } }>
+          
+          {/* CATEGORY HORIZONTAL TRUE */}
+          <View style={ {  flexDirection: "row", marginTop: 0 } }>
+            <ScrollView horizontal={ true } 
+            style={ styles.ScrollView } >
+              
+              {
+                categories.map((category, index)=> (
+                  <Button style={ styles.Button }
+                    buttonStyle={ this.buttonSeleted(index) }
+                    title= { category.name }
+                  ></Button>
+                ))
+              }
+              
+            </ScrollView>
+          </View>
+
+          {/* POSTS IN CATEGORY */}
+          <View style={ { flexDirection: 'column' } }>
+              <ScrollView horizontal={ false } >
+              {
+                news.map((newDetail, i)=> (
+                  <ListItem key={i} bottomDivider>
+                  <Avatar source= 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg' />
+                  <ListItem.Content>
+                    <ListItem.Title>{newDetail.name}</ListItem.Title>
+                    <ListItem.Subtitle>{newDetail.description}</ListItem.Subtitle>
+                  </ListItem.Content>
+                </ListItem>
+                ))
+              }
+              </ScrollView>
+          </View>
+
+        </View>
+
 
       </ThemeProvider>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  Button: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 15
+  },
+  ButtonStyle: {
+    borderRadius: 50,
+    backgroundColor: '#ccc'
+  },
+  ButtonStyleSelected: {
+    borderRadius: 50,
+    backgroundColor: '#606060'
+  },
+  ScrollView: {
+    marginHorizontal: 10,
+    borderTopLeftRadius: 50,
+    borderBottomLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50
+  }
+});
